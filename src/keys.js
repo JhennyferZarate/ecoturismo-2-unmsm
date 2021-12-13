2feb7027bc053902f55ebdfbd1298cab5dded216
arnold <luizarnolch@gmai.com>
2021-12-13 17:59:39 -0500

    heroku

---
+++ src/keys.js
@@ -0,0 +1,23 @@
+module.exports = {
+    // LOCAL
+    database: {
+        host: `us-cdbr-east-05.cleardb.net`,
+        user: `ba2c0bb268472d`,
+        password: `a0d5ee23`,
+        database: `heroku_6e26633d6613594`
+    }
+    // AMBIENTE TEST
+    /*
+    database: {
+        host: "buafjdbqroblshuld81t-mysql.services.clever-cloud.com",
+        user: "uqbrgdc9bht3sd2e",
+        password: "U4TsdS5Z7JqYOvgdefH8",
+        database: "buafjdbqroblshuld81t"
+    }
+
+    MYSQL_HOST       = us-cdbr-east-05.cleardb.net
+    MYSQL_USER       = ba2c0bb268472d
+    MYSQL_PASSWORD   = a0d5ee23
+    MYSQL_DATABASE   = heroku_6e26633d6613594
+    */
+};
